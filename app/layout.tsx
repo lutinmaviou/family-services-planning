569bc3c5a6c772886f1973d747107b1b92e08bfc
Bertrand Bourion <bertrandbou@hotmail.fr>
2024-01-13 14:42:54 +0100

    test mobile iphone SE 2020 size in deployement

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,14 +3,17 @@ import './globals.scss';
 
 export const metadata: Metadata = {
   manifest: '/manifest.json',
-  title: 'Family Services Planning',
-  description: 'Mon planning Family Services',
+  title: 'Family Services',
+  description: 'Mon suivi planning Family Services',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr">
-      <body suppressHydrationWarning={true}>{children}</body>
+      <body suppressHydrationWarning={true}>
+        {children}
+        <h1>Family Services</h1>
+      </body>
     </html>
   );
 }
